96dacf104c3b5a8655eeee763700f704e8a5680f
Ruben Torreblanca <ruben_torreblanca@hotmail.com>
2025-02-12 18:58:17 -0600

    Se creo la funcion para agregar amigos y falta crear la lista de amigos que debe aparecer

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos=[];
+
+function agregarAmigos (){
+    let nombres= document.getElementById('amigo').value;
+    if ( nombres != '') {
+        amigos.push(nombres);
+        console.log(amigos);
+    } else {
+        alert ("Ingresa un nombre correcto");
+    }
+    document.querySelector('#amigo').value='';
+}
+
+function  listaDeAmigos() {
+    document.getElementById('listaAmigos').value;
+}
