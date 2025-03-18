5decc0b75968064e8cb71d51f50324d46b976c66
Ruben Torreblanca <ruben_torreblanca@hotmail.com>
2025-03-18 17:59:59 -0600

    Ultimas actualizaciones

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos=[];
+let amigoElegido=[];
 
 function agregarAmigos (){
-    let nombres= document.getElementById('amigo').value;
-    if ( nombres != '') {
-        amigos.push(nombres);
-        console.log(amigos);
+    let nombres = document.getElementById('amigo');
+    let amigo = nombres.value;
+    if ( amigo != '') {
+        amigoElegido.push(amigo);
+        console.log(amigoElegido);
     } else {
         alert ("Ingresa un nombre correcto");
+        return;
     }
     document.querySelector('#amigo').value='';
+    crearLista();
 }
 
-function  listaDeAmigos() {
-    document.getElementById('listaAmigos').value;
+function  crearLista() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML="";
+
+    for (let i=0; i < amigoElegido.length; i++) {
+        let item = document.createElement("li");
+        item.textContent = amigoElegido[i];
+        listaAmigos.appendChild(item);
+    }
+}
+
+function sortearAmigo() {
+    if (amigoElegido.length < 2) {
+        alert ("Ingresa más de un amigo")
+        return;
+    }
+    let sorteo = amigoElegido[Math.floor(Math.random()*amigoElegido.length)];
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `El amigo amigo elegido es: ${sorteo}`;
+
+    let limpiarLista = document.getElementById('listaAmigos');
+    limpiarLista.innerHTML = "";
 }
---
+++ readme.md
@@ -0,0 +1,5 @@
+<h1> Información importante del proyecto "Amigo secreto" </h1>
+<h2>
+En este proyecto se realiza aplicación que te permite ingresar una lista de nombres de amigos con un boton interactivo, posteriormente con otro boton se elegirá de manera aleatoria uno de los nombres el cual será el amigo secreto.
+Este proyecto se realiza por medio de java script para desarrollar la logia de la aplicación y con HTML y CSS para la interfaz.
+</h2>
